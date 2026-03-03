d5d4f826f4bd852de59bef424131bcbe41d6a58e
Juan Diego Arias <Juandiegoarias1014@gmail.com>
2026-03-02 21:05:00 -0500

    actualizado final

--- proyecto/services/catalog-service_malo/src/main/java/com/ejemplo/catalogservice/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ejemplo.catalogservice.repository;
-
-import com.ejemplo.catalogservice.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
-}
